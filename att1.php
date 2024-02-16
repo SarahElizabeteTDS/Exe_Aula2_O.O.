529cd3d68ecfd3b263018cc0431562ed7cedda3e
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-02-16 14:22:57 -0300

    Update att1.php

--- att1.php
+++ att1.php
@@ -2,7 +2,7 @@
 
     for ($i = 0; $i < 10; $i++) 
     { 
-        $num = readline("Informe um número: ");
+        $num = (int) readline("Informe um número: ");
         if ($num >= 0) 
         {
             print "O número " . $num . " é positivo!\n";
